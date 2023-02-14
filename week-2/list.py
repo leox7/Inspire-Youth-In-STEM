34bd542f9aee2620c4dca5e63b99b8585008a5f3
leox7 <leonjohn397@gmail.com>
2023-02-14 10:41:47 +0300

    lists in python

---
+++ week-2/list.py
@@ -0,0 +1,23 @@
+names=["leon","waweru","john","kamau","jay","shan"]
+#accessing items in a list 
+print(names)
+print(names[0])
+print(names[-1])
+print(names[2])
+print(names[0:3])
+#create a list of fruits 
+fruits = ["apple","watermelon","pear","banana","orange","kiwi","avocados"]
+print(fruits)
+print(fruits[3])
+print("my favourite fruits is:",fruits[-1].upper())
+#adding two lists
+vegetables = ["kales","spinnach","cabbage","carrots","brocoli"]
+stationary = ["pens","ruler","pencils","rubber","stapler"]
+shoppings = (vegetables) + (stationary)
+print(shoppings)
+print(shoppings[5])
+for vegetable in vegetables:
+    print(vegetable)    
+    for shopping in shoppings:
+        print(shopping)
+print("My name is:" + names[0] + "and my favourite fruit is:" + fruits[-1])       
