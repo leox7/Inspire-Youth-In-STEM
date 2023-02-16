c32769b7e053ea1c1f3b609196d198e0da59348e
leox7 <leonjohn397@gmail.com>
2023-02-16 10:35:24 +0300

    square_cubes_mean

---
+++ week-2/data_types.py
@@ -0,0 +1,25 @@
+#floating vs non floating
+weight = 79.7 #kgs decimal point
+maths_grade = 72 #non floating
+x = 20
+y = 5
+
+
+
+#characters - a b c...
+chem_grade = "D"
+bio_grade = "B-"
+#arithmetic operations 
+
+z = x + y # addition
+p = x - y #subtruction
+q = x / y #divide
+m = x % y #remainder
+square_x = x**2 
+cube_x = x**3
+
+
+
+number = 13
+print("square of number",number**2)
+print("cube of number",number**3)
