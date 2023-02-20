c19e73bc24192ed8cb91425be1ee0951b8ba5eb0
leox7 <leonjohn397@gmail.com>
2023-02-20 10:41:39 +0300

    income

--- week-2/bank.py
+++ week-2/bank.py
@@ -5,7 +5,7 @@
 #Name : Leon John 
 #Email : leonjohn397@gmail.com
 #Date ; 17th Feb 2023
-#File :operators.py
+#File :bank.py
 
 
 # write a program that calculates 16% tax on income ranging 100k- 150k
@@ -19,19 +19,20 @@
 
 
 
-net_income=int(input("Enter your net income:"))
+gross_income=int(input("Enter your gross income:"))
 
 #above 300k
-if (net_income>=300000):
-    gross_income=-net_income + ((30/100)*net_income)
-    print("Since your net income is{} your gross income is{}".format(net_income,int(gross_income)))
+if (gross_income>=300000):
+    net_income= gross_income - ((30/100)*gross_income)
+    print(" if your gross income is:{} your net income is{}".format(gross_income,int(net_income)))
 
 #150k to less than 300k
-if (net_income>=150000) & (net_income <300000):
-    gross_income=net_income +((19/100)*net_income)
-    print("Since your income is {} your gross income {}".format(net_income,int(gross_income)))
+if (gross_income>=150000) & (gross_income <300000):
+    net_income=gross_income - ((19/100)*gross_income)
+    print(" if your gross income is:{} your net income is{}".format(gross_income,int(net_income)))
 
 
-if(net_income >= 1) & (net_income < 100000):
-	gross_income = net_income + ((5/100)*net_income)
-	print("Since Your Net income is {} your Gross income is {}".format(net_income, int(gross_income)))
+if(gross_income >= 1) & (gross_income < 100000):
+	net_income = gross_income - ((5/100)*gross_income)
+	print(" if your gross income is:{} your net income is{}".format(gross_income,int(net_income)))
+        
